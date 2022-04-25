237817c32b910d2e9975e926da3f5ec1d9dc0c59
Sevdi Merve Eroğlu <sevdimerve@gmail.com>
2022-04-25 18:41:01 +0300

    change footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="w-full box-border p-5 mt-3 text-center">
     <p class="text-sm">
-      Windzo Made by ❤️ Mohammad Sahrullah
+      News by Made Sevdi Merve Eroğlu
     </p>
   </footer>
 </template>
